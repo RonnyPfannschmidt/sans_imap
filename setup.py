aec4fc103e695ea0b0b2fdf30f5f96c5e3043575
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-07-04 22:35:53 +0200

    wip - new setup

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
 setup(
-    name='sans_imap',
-    description='an io-free implementation of imap',
-    setup_requires=['setuptools_scm'],
     use_scm_version=True,
-    license="MPLv2",
-    keywords=["testing"],
-    url="https://github.com/RonnyPfannschmidt/sans_imap",
+
     packages=[
         "sans_imap",
         "sans_imap.protocol",
